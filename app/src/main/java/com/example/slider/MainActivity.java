b51de8ba916d1b9e20fdd5ce866a16410f3ed750
Nagashima <wesley.nagashima98@gmail.com>
2022-03-30 21:56:23 -0300

    Initial commit

---
+++ app/src/main/java/com/example/slider/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.slider;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.heinrichreimersoftware.materialintro.app.IntroActivity;
+import com.heinrichreimersoftware.materialintro.slide.FragmentSlide;
+import com.heinrichreimersoftware.materialintro.slide.SimpleSlide;
+
+public class MainActivity extends IntroActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_main);
+        addSlide(new FragmentSlide.Builder()
+                .background(android.R.color.white)
+                .fragment(R.layout.intro_1)
+                .build());
+
+        addSlide(new FragmentSlide.Builder()
+                .background(android.R.color.white)
+                .fragment(R.layout.intro_2)
+                .build());
+
+        setButtonBackVisible(false);
+        setButtonNextVisible(false);
+        /*addSlide(new SimpleSlide.Builder()
+                .title("Titulo")
+                .description("Descrição")
+                .image(R.drawable.um)
+                .background(android.R.color.holo_orange_light)
+                .build());
+
+        addSlide(new SimpleSlide.Builder()
+                .title("Titulo2")
+                .description("Descrição2")
+                .image(R.drawable.dois)
+                .background(android.R.color.holo_orange_light)
+                .build());*/
+
+    }
+}
